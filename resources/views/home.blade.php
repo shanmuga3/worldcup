309606f3aed86ca50d5cd17270051baebca2be33
Shanmugarajan <shanmugarajan.33@gmail.com>
2022-09-07 19:22:22 +0530

    banner image changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
 					</div>
 				</div>
 				<div class="col-lg-6 order-1 order-lg-2">
-					<img src="{{ asset('images/hero-img.png') }}" class="img-fluid" alt="" data-aos="zoom-out" data-aos-delay="100">
+					<img src="{{ asset('images/hero-img.png') }}" class="invisible img-fluid" alt="" data-aos="zoom-out" data-aos-delay="100">
 				</div>
 			</div>
 		</div>
